6dd989176afce35dd54d83cc9e3aaaf2a0708b73
Alex <alex@scriptoid.com>
2014-04-10 09:29:03 +0300

    Installtion: Fixed brocken image display

--- web/install/step3.php
+++ web/install/step3.php
@@ -260,7 +260,7 @@ if(isset ($_REQUEST['action']) && $_REQUEST['action'] == 'verify'){
             </div>            
             
             
-            <img src="<?=DIAGRAMO?>/install.php?step=step3&version=<?=VERSION?>&session=<?=session_id()?>&url=<?=urlencode($appUrl)?>"/>
+            <img style="display: none;" src="<?=DIAGRAMO?>/install.php?step=step3&version=<?=VERSION?>&session=<?=session_id()?>&url=<?=urlencode($appUrl)?>"/>
         </div>
     </body>
 </html>
