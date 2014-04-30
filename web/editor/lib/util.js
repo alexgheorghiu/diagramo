f6a296582f05d326d6c0805384406eb092ce4b1a
PokatilovArt <none@none>
2014-04-30 11:12:32 +0300

    #116 (Util::rgbToHsl() added.) Bevel, light

--- web/editor/lib/util.js
+++ web/editor/lib/util.js
@@ -29,6 +29,45 @@ var Util = {
     },
 
 
+    /**
+     * Converts an RGB color value to HSL. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes r, g, and b are contained in the set [0, 255] and
+     * returns h, s, and l in the set [0, 1].
+     *
+     * @param {Number} r - The red color value
+     * @param {Number} g - The green color value
+     * @param {Number} b - The blue color value
+     * @return {Array<Number>} - the HSL representation
+     *
+     * Taken from: http://stackoverflow.com/a/9493060
+     */
+    rgbToHsl: function(r, g, b){
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        var max = Math.max(r, g, b);
+        var min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+
+        if(max === min){
+            h = s = 0; // achromatic
+        } else {
+            var d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch(max){
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+
+        return [h, s, l];
+    },
+
+
     /***/
     getUnionBounds: function(shapes){
         //tODo
