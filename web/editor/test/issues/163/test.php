ce47027d443dca18dfff4b548f2689f02a43f5d0
PokatilovArt <none@none>
2014-05-02 16:59:43 +0300

    #163 (Test file for proxy connection modified: variables renamed.) Installation Step2 failing on Internet connection through proxy

--- web/editor/test/issues/163/test.php
+++ web/editor/test/issues/163/test.php
@@ -1,17 +1,19 @@
 <?php
 // read proxy configuration from file
-$lines = parse_ini_file('proxy.ini');
+$proxyConfig = parse_ini_file('proxy.ini');
 
-// get user-defined address, port, authentification flag, login and password for proxy
-$useProxy = $lines['use_proxy'];
-$proxyAddress = $lines['proxy_address'];
-$proxyPort = $lines['proxy_port'];
-$proxyAuth = $lines['proxy_auth'];
-$proxyLogin = $lines['proxy_login'];
-$proxyPassword = $lines['proxy_password'];
+// get user-defined flag for proxy usage
+$useProxy = $proxyConfig['use_proxy'];
 
 // proxy enabled?
 if ($useProxy == '1') {
+    // get user-defined address, port, authorization flag, login and password for proxy
+    $proxyAddress = $proxyConfig['proxy_address'];
+    $proxyPort = $proxyConfig['proxy_port'];
+    $proxyAuth = $proxyConfig['proxy_auth'];
+    $proxyLogin = $proxyConfig['proxy_login'];
+    $proxyPassword = $proxyConfig['proxy_password'];
+
     // construct http context
     $aContext = array(
         'http' => array(
@@ -20,9 +22,9 @@ if ($useProxy == '1') {
         ),
     );
 
-    // proxy authentification enabled?
+    // proxy authorization enabled?
     if ($proxyAuth == '1') {
-        // set proxy authentification header
+        // set proxy authorization header
         $aContext['http']['header'] = "Proxy-Authorization: Basic " . base64_encode($proxyLogin . ':' . $proxyPassword);
     }
 
